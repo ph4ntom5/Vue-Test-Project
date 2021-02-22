2da73a4db66a445c46238234a809e74f7b72d147
ph4ntom5 <tomsstals1@gmail.com>
2021-02-22 22:06:44 +0100

    Product Section update

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    description: "Great socks, 5/5!!",
+    image: "./assets/green-sock.png",
+    altText: "A pair of socks",
+  },
+});
